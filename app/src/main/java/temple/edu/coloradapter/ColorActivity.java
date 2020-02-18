6bde59807aa070df5cfbbc16d9878711578d6597
Tuyen Pham <tuh39709@temple.edu>
2020-02-18 18:11:35 -0500

    create class color adapter extends BaseAdapter

--- app/src/main/java/temple/edu/coloradapter/ColorActivity.java
+++ app/src/main/java/temple/edu/coloradapter/ColorActivity.java
@@ -2,7 +2,11 @@ package temple.edu.coloradapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class ColorActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class ColorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
+        final Spinner colorSpinner = findViewById(R.id.colorSpinner);
+        final View activityLayout = findViewById(R.id.activityLayout);
+
+        ColorAdapter colorAdapter = new ColorAdapter(this, colors);
+        colorSpinner.setAdapter( colorAdapter );
+
+        colorSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                activityLayout.setBackgroundColor( Color.parseColor((String) parent.getItemAtPosition(position)) );
+                view.setBackgroundColor( Color.parseColor("white") );
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/temple/edu/coloradapter/ColorAdapter.java
+++ app/src/main/java/temple/edu/coloradapter/ColorAdapter.java
@@ -1,18 +1,30 @@
 package temple.edu.coloradapter;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 public class ColorAdapter extends BaseAdapter {
+
+    private Context context;
+    private String[] colors;
+
+    public ColorAdapter(Context context, String[] colors) {
+        this.context = context;
+        this.colors = colors;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return colors.length;
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return colors[i];
     }
 
     @Override
@@ -21,7 +33,28 @@ public class ColorAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView view;
+
+        if( convertView instanceof TextView ){
+            view = (TextView) convertView;
+        }else view = new TextView(context);
+
+        view.setText( colors[position] );
+        view.setTextSize(24);
+        view.setPadding(12,12,12,12);
+
+        return view;
     }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        TextView view;
+
+        view = (TextView) this.getView(position, convertView, parent);
+        view.setBackgroundColor(Color.parseColor( colors[position] ));
+
+        return view;
+    }
+
 }
